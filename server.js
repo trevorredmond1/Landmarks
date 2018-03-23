d4e7e0f7d798a25e6bc3c709445a4ba8bd2bceb4
waikitng <waikitng64@gmail.com>
2018-03-23 10:11:11 -0700

    started localhost server with main page

---
+++ server.js
@@ -0,0 +1,14 @@
+//constants
+const hbs = require('hbs');
+const express = require('express');
+
+//static content
+var app = express();
+app.use(express.static(__dirname + '/public'))
+
+//main page
+app.get('/main', (request, response) => {
+
+})
+
+app.listen(8080);
