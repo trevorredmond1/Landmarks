84da54e6a8f746e3c1cd0aa02fe2571035981c8d
Rindensan <videogamingawesomness@gmail.com>
2018-03-26 16:26:28 -0700

    Adding the Google Maps API to the repository

---
+++ googleapi.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+var getAddress = (address, callback) => {
+
+    request({
+        //url: 'http://maps.googleapis.com/maps/api/geocode/json?address=folwark%20leszcyznowka',
+        url: 'http://maps.googleapis.com/maps/api/geocode/json' +
+            '?address=' + encodeURIComponent(address),
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            callback('Cannot connect to Google Maps');
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback('Cannot find requested address');
+        } else if (body.status === 'OK') {
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                type: body.results[0].types[0]
+            });
+            //console.log(`Your requested venue: ${address}`);
+            //console.log(`Address: ${body.results[0].formatted_address}`);
+            //console.log(`Type: ${body.results[0].types[0]}`);
+        }
+    });
+
+};
+
+module.exports = {
+    getAddress
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //constants
 const hbs = require('hbs');
 const express = require('express');
+const geocode = require('request');
 
 //static content
 var app = express();
