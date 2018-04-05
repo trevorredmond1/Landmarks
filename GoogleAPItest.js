a1626634ecff2f85089f1bf439ba3494842404bb
Rindensan <videogamingawesomness@gmail.com>
2018-04-05 15:11:00 -0700

    Made some markers for each coordination

--- GoogleAPItest.js
+++ GoogleAPItest.js
@@ -4,7 +4,7 @@
     <style>
       #map {
         height: 400px;
-        width: 400px;
+        width: 100%;
        }
     </style>
   </head>
@@ -14,19 +14,25 @@
     <script>
       function initMap() {
         var marker =""
-        var uluru = [{lat: -25.363, lng: 131.044},{lat: -33.857, lng: 151.215}];
+        var locations = [{lat: 51.4968, lng: -115.9281},{lat: 43.6426, lng: -79.3871},{lat: 49.3017, lng: -123.1417},{lat: 40.7829, lng: -73.9654},{lat: 37.8199, lng: -122.4783},{lat: 40.6892, lng: -74.0445}];
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
-          center: {lat: -25.363, lng: 131.044}
+          center: {lat: 51.4968, lng: -115.9281}
         });
 
-        for (var i =0; i < uluru.length; i++){
+        for (var i =0; i < locations.length; i++){
         marker = new google.maps.Marker({
-          position: uluru[i],
+          position: locations[i],
           map: map
         });
       }
       }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGT9DtN2cOFyQW8CY5GD7cHYbeWsBoox8&callback=initMap">
+    </script>
+  </body>
+</html>
     </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGT9DtN2cOFyQW8CY5GD7cHYbeWsBoox8&callback=initMap">
