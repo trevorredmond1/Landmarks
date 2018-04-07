9291aebcc831e155b5bf56e26196f228acd3142c
Rindensan <videogamingawesomness@gmail.com>
2018-04-07 16:22:22 -0700

    The images should work now

--- googleapi.js
+++ googleapi.js
@@ -3,13 +3,13 @@ var Countries= {Canada: {center: {
                             },
                          location1: {
                             coor: {lat: 51.4968, lng: -115.9281},
-                            picture: url(picture/banffpark.jpg)},
+                            picture: "picture/banffpark.jpg"},
                          location2: {
                             coor: {lat: 43.6426, lng: -79.3871},
-                            picture: url(picture/cntower.jpg)},
+                            picture: "picture/cntower.jpg"},
                          location3: {
                             coor: {lat: 49.3017, lng: -123.1417},
-                            picture: url(picture/stanleypark.jpg)},
+                            picture: "picture/stanleypark.jpg"},
                         }
                }
 
@@ -45,12 +45,19 @@ function changeMap(center, location1, location2, location3) {
       }
 
 function changeImage(div, image){
-    div.image = image
-}
+  if (div == 0){
+    document.getElementById('firstcountry').style.image = "url(" + image + ")"}
+  }
+  else if (div == 1){
+    document.getElementById('secondcountry').style.image = "url(" + image + ")"}
+  }
+  else if (div == 2){
+    document.getElementById('thirdcountry').style.image = "url(" + image  + ")"}
+  }
 
 document.getElementById('Canada').addEventListener("click", function(){
-    changeMap(Countries.Canada.center.coor,Countries.Canada.location1.coor,Countries.Canada.location2.coor, Countries.Canada.location3.coor)
+    changeMap(Countries.Canada.center.coor,Countries.Canada.location[1].coor,Countries.Canada.location[2].coor, Countries.Canada.location[3].coor)
     for (var i = 0; i < 3; i++){
-        changeImage(i, Countries.Canada.location+ i +.picture)
+        changeImage(i, Countries.Canada.locations[i].picture)
     }
 });
