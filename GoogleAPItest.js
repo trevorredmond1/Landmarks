7d0e7524cdfd4ea75c2dca5dcb5feb230b01805a
Rindensan <videogamingawesomness@gmail.com>
2018-04-08 17:40:25 -0700

    nothing of importance

--- GoogleAPItest.js
+++ GoogleAPItest.js
@@ -14,25 +14,19 @@
     <script>
       function initMap() {
         var marker =""
-        var locations = [{lat: 51.4968, lng: -115.9281},{lat: 43.6426, lng: -79.3871},{lat: 49.3017, lng: -123.1417},{lat: 40.7829, lng: -73.9654},{lat: 37.8199, lng: -122.4783},{lat: 40.6892, lng: -74.0445}];
+        var locations = [{lat: 51.4968, lng: -115.9281},{lat: 43.6426, lng: -79.3871},{lat: 49.3017, lng: -123.1417},{lat: 40.7829, lng: -73.9654},{lat: 37.8199, lng: -122.4783},{lat: 40.6892, lng: -74.0445},{lat: 55.7520, lng: 37.6175},{lat: 55.7539, lng: 37.6208}, {lat:59.9398, lng: 30.3146}];
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: {lat: 51.4968, lng: -115.9281}
         });
 
-        for (var i =0; i < locations.length; i++){
-        marker = new google.maps.Marker({
-          position: locations[i],
-          map: map
-        });
-      }
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGT9DtN2cOFyQW8CY5GD7cHYbeWsBoox8&callback=initMap">
-    </script>
-  </body>
-</html>
+  for (var i =0; i < locations.length; i++){
+  marker = new google.maps.Marker({
+    position: locations[i],
+    map: map
+  });
+}
+}
     </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGT9DtN2cOFyQW8CY5GD7cHYbeWsBoox8&callback=initMap">
