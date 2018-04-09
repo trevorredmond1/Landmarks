64e7f1f28a916d72b40e77d098e309ed5a9c53fa
trevorredmond1 <trevor.redmond19@gmail.com>
2018-04-09 02:10:13 -0700

    df

--- save.js
+++ save.js
@@ -1,9 +1,16 @@
-const fs = require('fs');
+var fs = require('fs');
 
-module.exports.add = function(name, location) {
+var email = 'trevor@email.com';
+var location = 'Golden Gate Bridge';
+
+var add = (name) => {
 	var read = fs.readFileSync('data.JSON');
 	var parse = JSON.parse(read);
-	var add = {"email": name, "location": location};
+	var add = `${name}: {"location": []}`;
 	parse.push(add);
 	fs.writeFileSync('filetest.json', JSON.stringify(parse));
 };
+
+//module.exports.add = function(name) {
+
+add(email);
